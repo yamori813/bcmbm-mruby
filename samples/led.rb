be7197cd54ea8df95c704ee1be1c462d255e3fe7
yamori813 <yamori813@yahoo.co.jp>
2019-12-30 08:03:16 +0900

    gpio check script update

--- samples/led.rb
+++ samples/led.rb
@@ -6,17 +6,26 @@ bit = 0
 
 maxpin = 8
 
+orgdir = yabm.gpiogetdir()
+orgctl = yabm.gpiogetctl()
+yabm.print "ORG DIR: " + orgdir.to_s + " CTL: " + orgctl.to_s + "\r\n"
+yabm.gpiosetdir(0xff)
+
 while 1 do
 
-  yabm.print bit.to_s + "\n"
+  yabm.print bit.to_s + " "
   yabm.gpiosetdat(1 << bit)
   bit = bit + 1
   if bit == maxpin
     bit = 0
   end
-  start = yabm.count() 
-  while yabm.count() < start + 10000 do
-  end
+  6.times {
+    start = yabm.count() 
+    while yabm.count() < start + 1000 do
+    end
+    yabm.print "*"
+  }
+  yabm.print "\r\n"
 end
 
 rescue => e
