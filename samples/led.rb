be1bacb270a3a8806ece0c3cc7a7305570b0a0de
Hiroki Mori <yamori813@yahoo.co.jp>
2023-09-30 05:54:48 +0900

    ruby like script

--- samples/led.rb
+++ samples/led.rb
@@ -2,31 +2,28 @@ begin
 
 yabm = YABM.new
 
-bit = 0
-
-maxpin = 8
+pins = [0, 1, 2, 3, 4, 5, 6, 7]
 
 orgdir = yabm.gpiogetdir()
 orgctl = yabm.gpiogetctl()
 yabm.print "ORG DIR: " + orgdir.to_s + " CTL: " + orgctl.to_s + "\r\n"
 yabm.gpiosetdir(0xff)
 
-while 1 do
+loop do
+  pins.each do |n|
+    yabm.print n.to_s + ": "
 
-  yabm.print bit.to_s + ": "
-  5.times {
-    yabm.print "1"
-    yabm.gpiosetdat(1 << bit)
-    yabm.msleep(1000)
-    yabm.print "0"
-    yabm.gpiosetdat(0)
-    yabm.msleep(1000)
-  }
-  bit = bit + 1
-  if bit == maxpin
-    bit = 0
+    5.times {
+      yabm.print "1"
+      yabm.gpiosetdat(1 << n)
+      yabm.msleep(1000)
+      yabm.print "0"
+      yabm.gpiosetdat(0)
+      yabm.msleep(1000)
+    }
+
+    yabm.print "\r\n"
   end
-  yabm.print "\r\n"
 end
 
 rescue => e
