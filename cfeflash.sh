1509f172bdfbea5fd689cf55b17ca10b0e6eeec5
Hiroki Mori <yamori813@yahoo.co.jp>
2025-12-19 14:55:20 +0900

    NetBSD support. But not good

--- cfeflash.sh
+++ cfeflash.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-if [ $# == 2 ]; then
+if [ $# -eq 2 ]; then
 
 IP=$1
 FILE=$2
 
+OS=`uname`
+
+if [ ${OS} = "FreeBSD" ]; then
+OPT="-t 1 -c2"
+else
+OPT="-w 1 -c2"
+fi
+
 while [ 1 ]
 do
-ping -t 1 -c 2 ${IP} >/dev/null 2>&1
-if [ $? == 0 ]; then
+ping ${OPT} ${IP} >/dev/null 2>&1
+if [ $? -eq 0 ]; then
 echo " Find target"
 echo "bin
 put ${FILE}
